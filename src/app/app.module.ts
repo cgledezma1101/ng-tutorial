4474db457d0733bf50100a1d7d4c965ad828eea5
Carlos Ledezma <cgledezma1101@gmail.com>
2017-07-15 14:36:48 +1000

    Added navigation to hero details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,7 +33,12 @@ import { HeroService } from './hero.service';
 				path: '',
 				redirectTo: '/dashboard',
 				pathMatch: 'full'
+			},
+			{
+				path: 'detail/:id',
+				component: HeroDetailComponent
 			}
+
 		])
 	],
 	providers: [HeroService],
