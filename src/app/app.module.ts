7b53f1e3da230f4dd414084af8487f4b2226e615
Carlos Ledezma <cgledezma1101@gmail.com>
2017-07-15 14:15:04 +1000

    Added navigation to basic Dashboard page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { HeroDetailComponent } from './hero-detail.component';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service'
+import { DashboardComponent } from './dashboard.component';
+
+import { HeroService } from './hero.service';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HeroDetailComponent,
-		HeroesComponent
+		HeroesComponent,
+		DashboardComponent
 	],
 	imports: [
 		BrowserModule,
-		FormsModule
+		FormsModule,
+		RouterModule.forRoot([
+			{
+				path: 'heroes',
+				component: HeroesComponent
+			},
+			{
+				path: 'dashboard',
+				component: DashboardComponent
+			},
+			{
+				path: '',
+				redirectTo: '/dashboard',
+				pathMatch: 'full'
+			}
+		])
 	],
 	providers: [HeroService],
 	bootstrap: [AppComponent]
