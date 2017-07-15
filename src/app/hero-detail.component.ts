9ffebb663ab9cd8b0d2841478a5b4505859531a6
Carlos Ledezma <cgledezma1101@gmail.com>
2017-07-15 15:17:51 +1000

    Added styles for the  application

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -10,7 +10,8 @@ import 'rxjs/add/operator/switchMap';
 
 @Component({
 	selector: 'hero-detail',
-	templateUrl: './hero-detail.component.html'
+	templateUrl: './hero-detail.component.html',
+	styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
 	// The @Input decorator is necessary to allow the hero to be provided to the component as a parameter in the markup:
