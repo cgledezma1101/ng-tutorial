cc4b60d00a13d3a5c91cd03b9c89ec7935e1110f
Carlos Ledezma <cgledezma1101@gmail.com>
2017-07-29 19:09:32 +1000

    Included mock data service and retrieved heroes from service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { HeroDetailComponent } from './hero-detail.component';
 import { AppComponent } from './app.component';
@@ -11,6 +12,9 @@ import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
 	declarations: [
@@ -22,6 +26,8 @@ import { AppRoutingModule } from './app-routing.module';
 	imports: [
 		BrowserModule,
 		FormsModule,
+		HttpModule,
+		InMemoryWebApiModule.forRoot(InMemoryDataService),
 		AppRoutingModule
 	],
 	providers: [HeroService],
